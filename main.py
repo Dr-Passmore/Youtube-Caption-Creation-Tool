98ba36daf5f4269f463f7a37eb1aa72a19658e3d
Phillip Passmore <phillipspassmore@gmail.com>
2023-09-06 18:04:05 +0100

    config check added
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- main.py
+++ main.py
@@ -7,7 +7,44 @@ import requests
 
 class captionCreation():
     def __init__(self):
-        pass
+        config = configparser.ConfigParser()
+        configFile = os.path.exists('config.ini')
+        if configFile == False:
+            #if the file does not exist then create one
+            logging.error("No config file found")
+            captionCreation.configCreation(self, config)
+        else:
+            pass
+    
+    def configCreation(self, config):
+        """
+        Creates a configuration file 'config.ini' if it doesn't exist.
+
+        Input:
+        - config: ConfigParser instance for handling configuration.
+
+        Output:
+        - None (Creates or updates the 'config.ini' file.)
+        """
+        logging.info("Creating 'config.ini' file")
+        try:
+            apiKey = input("Please enter your API key:\n")
+            config.add_section('API Key')
+            config.set('API Key', 'Key', apiKey)
+            with open(r"config.ini", 'w') as configuration:
+                config.write(configuration)
+            logging.info('config.ini created successfully')
+        except Exception as e:
+            logging.error(f"Failed to create config.ini: {e}")
+            
+        
+logging.basicConfig(filename='CaptionGeneration.log', 
+                    filemode='a', 
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+        
     
     
     
