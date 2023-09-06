fad076ba19e21f801d9a1d1cb8b5610120ac264e
Phillip Passmore <phillipspassmore@gmail.com>
2023-09-06 09:28:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as ttk
+import configparser
+import logging
+import os
+import pvleopard
+import requests
+
+class captionCreation():
+    def __init__(self):
+        pass
+    
+    
+    
+if __name__ == "__main__":
+    captionCreation()
