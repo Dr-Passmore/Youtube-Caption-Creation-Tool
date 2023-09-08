85be0f10ac3a7c642566dfd5289d80ef814bab96
Dr-Passmore <phillipspassmore@gmail.com>
2023-09-08 20:57:00 +0100

    MP3 now downloading using Youtube title

--- mp3download.py
+++ mp3download.py
@@ -1,7 +1,6 @@
 import logging
 from pytube import YouTube
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import re
 import time
 
 class downloader:
@@ -10,46 +9,55 @@ class downloader:
         
 
     def download (url):
+        '''
+        Downloads the MP3 of the provided url
+
+        Input:
+        - url: YouTube URL 
         
+        Output:
+        - filename: generated from video title
+        '''
+        
+        #TODO Find a solution for age restricted content
         try:
-            yt = YouTube(
-                    url,
-            )
+            print("check")
+            yt = YouTube(url)
+            time.sleep(5)
+            name = yt.title
+            print("check")
+            print(name)
+            filename = downloader.remove_special_characters(name)
+            mp3file = f"{filename}.mp3"
+            
             stream = yt.streams.filter(only_audio=True).order_by('bitrate') \
             .last()
-            stream.download(filename=f"test3.mp3")
+            stream.download(filename=f"{filename}.mp3")
+            
+            return mp3file
         except Exception as e:
             error_message = str(e)
             if "age restricted" in error_message.lower():
                 print("The video is age-restricted and cannot be accessed without logging in.")
-                '''
-                 # Set up a Selenium WebDriver
-                driver = webdriver.Chrome()  # You can use other browsers by specifying the driver.
-
-                # Log in to YouTube
-                driver.get("https://www.youtube.com")
-                time.sleep(5)
-                driver.find_element_by_name("identifier").send_keys(username)
-                driver.find_element_by_id("identifierNext").click()
-                driver.implicitly_wait(5)
-                driver.find_element_by_name("password").send_keys(password)
-                driver.find_element_by_id("passwordNext").click()
-                driver.implicitly_wait(5)
-
-                # Access the age-restricted content
-                driver.get(url)
-
-                # Download the video using PyTube
-                yt = YouTube(url)
-                stream = yt.streams.filter(only_audio=True).first()
-                stream.download(filename="test2.mp3")
-                logging.info(f"Downloaded: {url}")
-
-                # Close the browser
-                driver.quit()
             else:
-                print(f"Error: {e}")
-            '''
+                logging.error(e)
+
+    def remove_special_characters(filename):
+        '''
+        Removes special characters from the filename
+
+        input: 
+        - filename: String
+
+        output:
+        - filename: String with special characters removed 
+        '''
+        pattern = r'[\\/:?"*|<>&%#$!,]'
+        
+        cleaned_string = re.sub(pattern, '', filename)
+        
+        return cleaned_string
+
 logging.basicConfig(filename='CaptionGeneration.log', 
                     filemode='a', 
                     level=logging.INFO,
@@ -59,4 +67,4 @@ logging.basicConfig(filename='CaptionGeneration.log',
 if __name__ == "__main__":
     downloader()
     
-downloader.download("https://www.youtube.com/watch?v=17Kszzcmfv4")
+downloader.download("https://youtu.be/fRRHDXHSsM0?si=n4nUNUyAVah88UZs")
